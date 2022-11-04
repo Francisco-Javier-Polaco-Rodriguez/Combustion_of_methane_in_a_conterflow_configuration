5af380872b1fc99949c1514ad5bf150648911ebd
Francisco-Javier-Polaco-Rodriguez <franciscopolancorodriguez@gmail.com>
2022-11-04 17:35:29 +0100

    First "functional" module. To check veracity by professor

---
+++ fluid_solver.py
@@ -0,0 +1,235 @@
+import numpy as np
+from colorama import Fore, Back, Style
+from tqdm import tqdm
+
+def DDx(M,dx,Bound_left = 0,Bound_right = 0):
+    I,J = M.shape
+    ddxM = np.zeros([I,J])
+    for j in range(1,J-1):
+        if j == J-1:
+            #ddxM[:,j] = (Bound_right-2*M[:,j]+M[:,j-1])/dx**2
+            ddxM[:,j] = 0 ## Intuition. No curvature on the derivative because free wall
+        elif j == 0:
+            ddxM[:,j] = (M[:,j+1]-2*M[:,j]+Bound_left)/dx**2
+        else:
+            ddxM[:,j] = (M[:,j+1]-2*M[:,j]+M[:,j-1])/dx**2
+    return ddxM
+
+def DDy(M,dy,Bound_up,Bound_down):
+    I,J = M.shape
+    ddyM = np.zeros([I,J])
+    for i in range(1,I-1):
+        if i == I-1:
+            ddyM[i,:] = (Bound_down-2*M[i,:]+M[i-1,:])/dy**2
+        elif i == 0:
+            ddyM[i,:] = (M[i+1,:]-2*M[i,:]+Bound_up)/dy**2
+        else:
+            ddyM[i,:] = (M[i+1,:]-2*M[i,:]+M[i-1,:])/dy**2
+    return ddyM
+
+def Dx(M,dx,Bound_left = 0,Bound_right = 0):
+    I,J = M.shape
+    dxM = np.zeros([I,J])
+    for j in range(1,J-1):
+        if j == J-1:
+            #ddxM[:,j] = (Bound_right-2*M[:,j]+M[:,j-1])/dx**2
+            dxM[:,j] = dxM[:,j-1] ## Intuition. No curvature on the derivative because free wall
+        elif j == 0:
+            dxM[:,j] = (M[:,j+1]-Bound_left)/dx
+        else:
+            dxM[:,j] = (M[:,j+1]-M[:,j-1])/dx
+    return dxM
+
+def Dy(M,dy,Bound_up,Bound_down):
+    I,J = M.shape
+    dyM = np.zeros([I,J])
+    for i in range(1,I-1):
+        if i == I-1:
+            dyM[i,:] = (Bound_down-M[i-1,:])/dy
+        elif i == 0:
+            dyM[i,:] = (M[i+1,:]-Bound_up)/dy
+        else:
+            dyM[i,:] = (M[i+1,:]-M[i-1,:])/dy
+    return dyM
+
+## Functions with no bc
+
+def DDx_nobc(M,dx):
+    I,J = M.shape
+    ddxM = np.zeros([I,J])
+    for j in range(1,J-1):
+        if j == J-1:
+            ddxM[:,j] = 0 ## Intuition. No curvature on the derivative because free wall
+        elif j == 0:
+            ddxM[:,j] = 0
+        else:
+            ddxM[:,j] = (M[:,j+1]-2*M[:,j]+M[:,j-1])/dx**2
+    return ddxM
+
+def DDy_nobc(M,dy):
+    I,J = M.shape
+    ddyM = np.zeros([I,J])
+    for i in range(1,I-1):
+        if i == I-1:
+            ddyM[i,:] = 0
+        elif i == 0:
+            ddyM[i,:] = 0
+        else:
+            ddyM[i,:] = (M[i+1,:]-2*M[i,:]+M[i-1,:])/dy**2
+    return ddyM
+
+def Dx_nobc(M,dx):
+    I,J = M.shape
+    dxM = np.zeros([I,J])
+    for j in range(1,J-1):
+        if j == J-1:
+            dxM[:,j] = dxM[:,j-1] ## Intuition. No curvature on the derivative because free wall
+        elif j == 0:
+            dxM[:,j] = dxM[:,j+1]
+        else:
+            dxM[:,j] = (M[:,j+1]-M[:,j-1])/dx
+    return dxM
+
+def Dy_nobc(M,dy,):
+    I,J = M.shape
+    dyM = np.zeros([I,J])
+    for i in range(1,I-1):
+        if i == I-1:
+            dyM[i,:] = dyM[i-1,:]
+        elif i == 0:
+            dyM[i,:] = dyM[i+1,:]
+        else:
+            dyM[i,:] = (M[i+1,:]-M[i-1,:])/dy
+    return dyM
+
+
+
+class fluid_initial_condition():
+    def __init__(self,u_0x,u_0y,p_0,viscosity,density):
+        self.ux = u_0x
+        self.uy = u_0y
+        self.pressure = p_0
+        self.t = np.array([])
+        self.dimensions = u_0x.shape
+        self.viscosity = viscosity
+        self.density = density
+        if p_0.shape != u_0x.shape or u_0x.shape != u_0y.shape:
+            raise ValueError('initial pressure and initial velocity must have the same shape')
+
+
+class boundary_condition():
+    def __init__(self,up,down):
+        self.up = up
+        self.down = down
+        self.dim = len(up)
+
+class pde_fluid_solver():
+    def __init__(self,fluid_ic,bc_ux,bc_uy,N_time,dt,Lx,Ly):
+        self.initial_condition = fluid_ic
+        self.bc_ux = bc_ux
+        self.bc_uy = bc_uy
+        self.Nt = N_time
+        self.dim = fluid_ic.dimensions
+        self.dt = dt
+        self.dx = Lx/self.dim[0]
+        self.dy = Ly/self.dim[0]
+        self.ux= fluid_ic.ux[:,:,np.newaxis]
+        self.uy = fluid_ic.uy[:,:,np.newaxis]
+        self.p = fluid_ic.pressure[:,:,np.newaxis]
+        if self.dim[0]!=bc_ux.dim or self.dim[0]!=bc_uy.dim:
+            raise ValueError('Boundary condition and fluid have not the same dimension')
+        # Stability comprobation
+        Fx = fluid_ic.viscosity*dt/self.dx**2
+        Fy = fluid_ic.viscosity*dt/self.dy**2
+        Cx = np.mean(np.mean(np.abs(fluid_ic.ux)+np.abs(fluid_ic.uy)))*dt/self.dx
+        Cy = Cx*self.dx/self.dy
+        if Fx > 0.25 or Fy > 0.25:
+            raise ValueError('Invalid fourier number. You need bigger grid or more little time step.  The stabilities parameters are  [Fx,Fy] = [%1.3f,%1.3f] [Cx,Cy]] =[%1.3f,%1.3f]'%(Fx,Fy,Cx,Cy))
+        if Cy > 1 or Cx > 1:
+            TypeError('Invalid C factor. You need dx/dt of the order of velocities. The stabilities parameters are  [Fx,Fy] = [%1.3f,%1.3f] [Cx,Cy]] =[%1.3f,%1.3f]'%(Fx,Fy,Cx,Cy))
+        print(Fore.BLUE + 'Solver pde class created successfully. The stabilities parameters are  [Fx,Fy] = [%1.3f,%1.3f] [Cx,Cy]] =[%1.3f,%1.3f]'%(Fx,Fy,Cx,Cy) + Style.RESET_ALL)
+    def presure_solver(self,left_side,right_side,precision = 0.05,max_reps = np.inf):
+        p = left_side.copy()
+        p_new = p.copy()
+        not_good = True
+        [I,J] = self.dim
+        repeats = 10
+        count = 0
+        old_rel_error = np.nan
+        while not_good and count < max_reps:
+            for k in range(1,repeats):
+                for i in range(1,I-1):## Jacobi solver with boundary condition p_0. No change p in boundary.
+                    for j in range(1,J-1):
+                        p_new[i,j] =+0.25*(p[i+1,j]+p_new[i-1,j]+p[i,j+1]+p_new[i,j-1])-0.25*self.dx**2*right_side[i,j]
+                del p
+                p = p_new.copy()
+                count += 1
+            
+            rel_error = np.mean(np.mean(np.abs(DDx(p_new,self.dx,0,0)+DDy(p_new,self.dy,0,0)-right_side),axis = 0),axis = 0)/np.mean(np.mean(np.abs(right_side),axis = 0),axis = 0)
+            if rel_error > precision and np.abs(rel_error-old_rel_error) < precision*0.1: ## Max accuracy of solver
+                not_good = False
+                print(Fore.RED + '\nWARNING: the relative error in pressure calculated by Jacobi method is %1.5f bigger than the precision = %1.5f. It is the better convergence that one can reach with  a %ix%i grid.'%(rel_error,precision,self.dim[0],self.dim[1]) + Style.RESET_ALL)
+            if rel_error <=precision: ## Control of the error
+                not_good = False
+            elif rel_error < 2*precision:
+                repeats = int(repeats/1.5)
+                if repeats < 50:
+                    repeats = 50
+            elif rel_error >= 10*precision:
+                repeats = int(repeats*2)
+            old_rel_error = rel_error
+        p = p_new
+        if repeats >= max_reps:
+            print(Fore.RED + '\nWARNING: the relative error in pressure calculated by Jacobi method is %1.5f bigger than the precision = %1.5f. It is the better convergence that one can reach with  %i repetitions of the method.'%(rel_error,precision,max_reps) + Style.RESET_ALL)
+        return p
+
+    def solve_navier_stokes(self,N = np.NaN,precision_jac = 0.1,repeat_jac = 100): # Advance N times Navier Stokes equations.
+        if N == np.NaN:
+            N = self.N_time
+        ## First step. Initialize simulation
+        ux = np.zeros([self.dim[0],self.dim[1],N])
+        uy = np.zeros([self.dim[0],self.dim[1],N])
+        p = np.zeros([self.dim[0],self.dim[1],N])
+        dt = self.dt
+        dx,dy = self.dx,self.dy
+        visc = self.initial_condition.viscosity
+        dens = self.initial_condition.density
+        ux[:,:,0] = self.ux[:,:,-1]
+        uy[:,:,0] = self.uy[:,:,-1]
+        p[:,:,0]  = self.p[:,:,-1]
+        # Preinicialize in RAM memory u star, u star stat
+        ux_s = np.zeros(self.dim)
+        ux_ss = np.zeros(self.dim)
+        uy_s = np.zeros(self.dim)
+        uy_ss = np.zeros(self.dim)
+        ## Load boundary contition of the problem up and down (velocities in the slot and coflow)
+        bc_x_up = self.bc_ux.up
+        bc_y_up = self.bc_uy.up
+        bc_x_down = self.bc_ux.down
+        bc_y_down = self.bc_uy.down
+        ## Empty matrix that is gonna be used
+        duxuy = np.zeros(self.dim)
+        ## Second step simulation !!!
+        for k in tqdm(range(1,N)):
+            uxy = ux[:,:,k-1]*uy[:,:,k-1]
+            duxuy = Dx(uxy,dx)+Dy(uxy,dy,Bound_up=bc_x_up*bc_y_up,Bound_down=bc_x_down*bc_y_down)
+            ux_s  = ux[:,:,k-1]-dt*duxuy
+            uy_s = uy[:,:,k-1]-dt*duxuy
+            ux_ss = ux_s+dt*visc*(DDx_nobc(ux_s,dx)+DDy_nobc(ux_s,dy))
+            uy_ss = uy_s+dt*visc*(DDx_nobc(ux_s,dx)+DDy_nobc(uy_s,dy))
+            p[:,:,k]  = self.presure_solver(p[:,:,k-1],dens*dt**-1*(Dx_nobc(ux_ss,dx)+Dy_nobc(uy_ss,dy)),max_reps=repeat_jac,precision=precision_jac)
+            ux[:,:,k] = ux_ss-dt*dens**-1*Dx_nobc(p[:,:,k],dx)
+            uy[:,:,k] = uy_ss-dt*dens**-1*Dy_nobc(p[:,:,k],dy)
+            # Boundary conditions
+            ux[0,:,k]  = bc_x_up
+            ux[-1,:,k] = bc_x_down
+            uy[0,:,k]  = bc_y_up
+            uy[-1,:,k] = bc_y_down
+
+            ux[:,0,k]  = np.zeros(self.dim[1])
+            # ux[:,-1,k] = nothing needed, free wall
+            # uy[:,0,k]  = nothing needed, slipping wall
+            # uy[:,-1,k] = nothing needed, free wall
+        self.ux = np.concatenate((self.ux,ux),axis = 2)
+        self.uy = np.concatenate((self.uy,uy),axis = 2)
+        self.p = np.concatenate((self.p,p),axis = 2)
---
+++ main.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from fluid_solver import *
+
+u_slot = 1
+u_coflow = 0.5
+N_space = 248
+N_time = 15
+Lx,Ly = 2e-3,2e-3
+dt = 25*(np.mean([u_slot,u_coflow])/Lx*N_space**2)**-1
+print(dt)
+[X,Y] = np.meshgrid(np.linspace(0,Lx,N_space),np.linspace(0,Lx,N_space))
+up_bc_uy = np.ones(N_space)
+down_bc_uy = np.ones(N_space)
+
+## Particula BC of the problem
+for k in range(N_space):
+    if k < N_space/4:
+        up_bc_uy[k] = -u_slot
+        down_bc_uy[k] = u_slot
+    elif N_space/4 <= k and k < N_space/2:
+        up_bc_uy[k] = -u_coflow
+        down_bc_uy[k] = u_coflow
+    else:
+        up_bc_uy[k] = 0
+        down_bc_uy[k] = 0
+
+up_bc_ux = np.zeros(N_space)
+down_bc_ux = np.zeros(N_space)
+
+## Initial conditions and object creation
+
+u0x = np.zeros([N_space,N_space])
+u0y = np.zeros([N_space,N_space])
+u0y[0,:]=down_bc_uy
+u0y[-1,:]=up_bc_uy
+u0x[0,:]=down_bc_ux
+u0x[-1,:]=up_bc_ux
+p0 = 101225*np.ones([N_space,N_space])
+
+
+viscosity,density = 15e-6,1.1614
+main_fluid = fluid_initial_condition(u_0x = u0x,
+                u_0y = u0y,
+                p_0 = p0,
+                viscosity = viscosity,
+                density = density)
+
+
+bc_ux = boundary_condition(up_bc_ux,down_bc_ux)
+bc_uy = boundary_condition(up_bc_uy,down_bc_uy)
+
+solver = pde_fluid_solver(main_fluid,bc_ux,bc_uy,N_time,dt,Lx,Ly)
+
+#solver.presure_solver(np.ones([N_space,N_space]),precision = 0.05,max_reps = 1000)
+solver.solve_navier_stokes(N_time,precision_jac = 0.05,repeat_jac = 150)
+
+
+# Change this to the path on your oun laptop
+path = '/Users/Pacopol/Desktop/Plasma Physics and Fusion Master/Numerical Methods/Project_fluid/Combustion_of_methane_in_a_conterflow_configuration/figures_for_videos'
+
+for k in np.arange(1,N_time,1):
+    fig = plt.figure()
+    plt.contour(X,Y,solver.p[:,:,k])
+    plt.colorbar
+    plt.quiver(X,Y,solver.ux[:,:,k],solver.uy[:,:,k])
+    fig.savefig(path + '/' + 'frame%i.png'%(k))
