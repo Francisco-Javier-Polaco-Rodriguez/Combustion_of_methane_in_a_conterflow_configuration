17d63232b8b9852a1b1ddb516cc82523aab6a224
Francisco-Javier-Polaco-Rodriguez <franciscopolancorodriguez@gmail.com>
2022-11-09 18:34:03 +0100

    Seems to work. To see acceleration
    Te most probable reason to see how the velocity goes to 25 in the exit is

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import os
 
 from fluid_solver import *
 
-u_slot = 10
-u_coflow = 5
+u_slot = 1
+u_coflow = 0.5
 N_x,N_y = 32,32
-N_time = 50
+N_time = 100
 Lx,Ly = 2e-3,2e-3
-dt = 1e-8
+dt = 100e-9
+viscosity,density = 15e-6,1.1614
 
 [X,Y] = np.meshgrid(np.linspace(0,Lx,N_x),np.linspace(0,Lx,N_y))
 up_bc_uy = np.ones(N_x)
@@ -41,7 +42,7 @@ u0x[-1,:]=down_bc_ux
 p0 = np.zeros([N_y,N_x])
 
 
-viscosity,density = 15e-4,1.1614
+
 main_fluid = fluid_initial_condition(u_0x = u0x,
                 u_0y = u0y,
                 p_0 = p0,
@@ -55,15 +56,21 @@ bc_uy = boundary_condition(up_bc_uy,down_bc_uy)
 solver = pde_fluid_solver(main_fluid,bc_ux,bc_uy,N_time,dt,Lx,Ly)
 
 #solver.presure_solver(np.ones([N_space,N_space]),precision = 0.05,max_reps = 1000)
-solver.solve_navier_stokes(N_time,precision_jac = 0.005,repeat_jac = 2000)
+solver.solve_navier_stokes(N_time,precision_jac = 0.005,repeat_jac = 20000)
 
 
 # Change this to the path on your oun laptop
 path = '/Users/Pacopol/Desktop/Plasma Physics and Fusion Master/Numerical Methods/Project_fluid/Combustion_of_methane_in_a_conterflow_configuration/figures_for_videos'
+X,Y = 1e3*X,1e3*Y
 
 for k in np.arange(1,N_time,1):
     fig = plt.figure()
-    plt.contour(X,Y,solver.p[:,:,k],cmap = 'jet')
-    plt.quiver(X,Y,solver.ux[:,:,k],solver.uy[:,:,k])
+    mod_u = np.sqrt(solver.ux[:,:,k]**2+solver.uy[:,:,k]**2)
+    color = plt.pcolor(X,Y,mod_u,cmap = 'jet')
+    plt.title('T = %1.2f ns'%(k*dt*1e6))
+    plt.quiver(X,Y,solver.ux[:,:,k]/mod_u,solver.uy[:,:,k]/mod_u)
+    plt.colorbar(color)
+    plt.xlabel('x   (mm)',size = 13)
+    plt.ylabel('y   (mm)',size = 13)
     fig.savefig(path + '/' + 'frame%i.png'%(k))
     del fig
